1bb6d94e2e189d9693310d74360ebab314d1f746
ammarqadas <ammar.alqadasi@gmail.com>
2026-07-03 02:44:05 +0300

    new desgin 2

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: 'standalone',
+  output: 'standalone',
   // basePath: '/',
   // Enable CORS for API routes
   async headers() {
